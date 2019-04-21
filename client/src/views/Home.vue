ee6b5f1dd0895e5b8c2ee5e86768e9398309718f
sequeirajonathan <sequeira.s.jonathan@gmail.com>
2019-04-20 22:56:55 -0400

    Initial Commit

---
+++ client/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">Welcome to Community!</h1>
+      <p class="lead">A fully open source forum made with Vue.JS.</p>
+      <hr class="my-4">
+      <p>This forum is all about community</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Sign up</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home"
+};
+</script>
